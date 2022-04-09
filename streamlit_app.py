3a9ffd9392da342c8fcf234bd9e86aa812e95049
Patricia Carrasco <patri.carrasco@gmail.com>
2022-04-09 12:15:05 +0200

    day5 done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,144 +1,34 @@
-# import streamlit as st
-
-
-# st.header('My first button')
-
-# if st.button('Say hello'):
-#      st.write('Why hello there')
-# else:
-#      st.write('Goodbye')
-
-
-import pandas as pd 
-import numpy as np 
-import plotly.graph_objects as go
-import plotly.express as px
+import numpy as np
+import altair as alt
+import pandas as pd
 import streamlit as st
-from datetime import datetime
 
 
-def style_negative(v, props=''):
-    """ Style negative values in dataframe"""
-    try: 
-        return props if v < 0 else None
-    except:
-        pass
-    
-def style_positive(v, props=''):
-    """Style positive values in dataframe"""
-    try: 
-        return props if v > 0 else None
-    except:
-        pass    
-    
-def audience_simple(country):
-    """Show top represented countries"""
-    if country == 'US':
-        return 'USA'
-    elif country == 'IN':
-        return 'India'
-    else:
-        return 'Other'
+st.header('st.write')
 
-@st.cache
-def load_data():
+st.write('Hello, *World!* :sunglasses:')
 
-     """ Loads in 4 dataframes and does light feature engineering"""
-     df_agg = pd.read_csv('./data/Aggregated_Metrics_By_Video.csv').iloc[1:,:]
-     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
-                         'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
-                         'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-     df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
-     df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
-     df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
-     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
-     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
-     df_agg.sort_values('Video publish time', ascending = False, inplace = True)    
-     df_agg_sub = pd.read_csv('./data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-     df_comments = pd.read_csv('./data/Aggregated_Metrics_By_Video.csv')
-     df_time = pd.read_csv('./data/Video_Performance_Over_Time.csv')
-     df_time['Date'] = pd.to_datetime(df_time['Date'])
-     return df_agg, df_agg_sub, df_comments, df_time 
+# Example 2
 
+st.write(1234)
 
-#create dataframes from the function 
-df_agg, df_agg_sub, df_comments, df_time = load_data()
+# Example 3
 
-#additional data engineering for aggregated data 
-df_agg_diff = df_agg.copy()
-metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months =12)
-median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median()
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
 
-#Just numeric columns 
-numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | (df_agg_diff.dtypes == 'int64'))
-df_agg_diff.iloc[:,numeric_cols] = (df_agg_diff.iloc[:,numeric_cols] - median_agg).div(median_agg)
+# Example 4
 
-#merge daily data with publish data to get delta 
-df_time_diff = pd.merge(df_time, df_agg.loc[:,['Video','Video publish time']], left_on ='External Video ID', right_on = 'Video')
-df_time_diff['days_published'] = (df_time_diff['Date'] - df_time_diff['Video publish time']).dt.days
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
-# get last 12 months of data rather than all data 
-date_12mo = df_agg['Video publish time'].max() - pd.DateOffset(months =12)
-df_time_diff_yr = df_time_diff[df_time_diff['Video publish time'] >= date_12mo]
+# Example 5
 
-# # get daily view data (first 30), median & percentiles 
-# views_days = pd.pivot_table(df_time_diff_yr,index= 'days_published',values ='Views', aggfunc = [np.mean,np.median,lambda x: np.percentile(x, 80),lambda x: np.percentile(x, 20)]).reset_index()
-# views_days.columns = ['days_published','mean_views','median_views','80pct_views','20pct_views']
-# views_days = views_days[views_days['days_published'].between(0,30)]
-# views_cumulative = views_days.loc[:,['days_published','median_views','80pct_views','20pct_views']] 
-# views_cumulative.loc[:,['median_views','80pct_views','20pct_views']] = views_cumulative.loc[:,['median_views','80pct_views','20pct_views']].cumsum()
-
-
-#create differences from the median for values 
-#Just numeric columns 
-numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | (df_agg_diff.dtypes == 'int64'))
-df_agg_diff.iloc[:,numeric_cols] = (df_agg_diff.iloc[:,numeric_cols] - median_agg).div(median_agg)
-
-#Start building Streamlit App
-add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics','Individual Video Analysis'))
-
-#Total picture
-if add_sidebar == 'Aggregate Metrics':
-     st.write("Patricia Aggregated Data")
-     df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
-                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
-     metric_date_6mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =6)
-     metric_date_12mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =12)
-     metric_medians6mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_6mo].median()
-     metric_medians12mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_12mo].median()
-
-     col1, col2, col3, col4, col5 = st.columns(5)
-     columns = [col1, col2, col3, col4, col5]
-     count = 0
-     for i in metric_medians6mo.index:
-        with columns[count]:
-            delta = (metric_medians6mo[i] - metric_medians12mo[i])/metric_medians12mo[i]
-            st.metric(label= i, value = round(metric_medians6mo[i],1), delta = "{:.2%}".format(delta))
-            count += 1
-            if count >= 5:
-                count = 0
-
- #get date information / trim to relevant data 
-     df_agg_diff['Publish_date'] = df_agg_diff['Video publish time'].apply(lambda x: x.date())
-     df_agg_diff_final = df_agg_diff.loc[:,['Video title','Publish_date','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
-                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
-     
-     
-     df_agg_numeric_lst = df_agg_diff_final.median().index.tolist()
-     df_to_pct = {}
-     for i in df_agg_numeric_lst:
-        df_to_pct[i] = '{:.1%}'.format
-
-     st.dataframe(df_agg_diff_final.style.applymap(style_negative, props='color:red;').applymap(style_positive, props='color:green;').format(df_to_pct))
-
-if add_sidebar == 'Individual Video Analysis':
-     videos = tuple(df_agg['Video title'])
-     video_select = st.selectbox('Pick A Video:', videos)
-
-     agg_filtered = df_agg[df_agg['Video title'] == video_select]
-     agg_sub_filtered = df_agg_sub[df_agg_sub['Video Title'] == video_select]
-     agg_sub_filtered['Country'] = agg_sub_filtered['Country Code'].apply(audience_simple)
-     agg_sub_filtered.sort_values('Is Subscribed', inplace= True)
-
-     fig = px.bar(agg_sub_filtered, x = 'Views', y='Is Subscribed', color ='Country', orientation='h')
-     st.plotly_chart(fig)
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
